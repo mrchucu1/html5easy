e8adbba5c1f61f3e71a3c9a28d118b08de1a7987
Diego Navarro <diegonavarro400@gmail.com>
2018-03-15 00:53:24 -0600

    Adding some sence into this proyect

--- index.php
+++ index.php
@@ -14,46 +14,46 @@
         </script>
     </head>
     <body class="html5e">
-        <header class="head" id="header" data-rightbar="true" data-padding="25px" data-max-width="900px" data-text="Do it yourself!" data-logo-src="private/img/web/logoHTML5e.png" data-text-align="right" data-logo-max-size="4em"></header>
+        <header
+                id="header"
+                class="head"
+                data-rightbar="true"
+                data-padding="25px"
+                data-max-width="900px"
+                data-text="Do it yourself!"
+                data-logo-src="private/img/web/logoHTML5e.png"
+                data-text-align="right"
+                data-logo-max-size="4em"
+        ></header>
         <nav data-config="nav2"></nav>
         <div class="content">
-            <aside class="anuncios">
-                
-            </aside>
+            <aside class="anuncios" data-type-add="true"></aside>
             <article class="start left">
                 <header>
                     <h1>It's better</h1>
                 </header>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet pellentesque urna, a tristique diam. Nulla posuere tempus turpis, id posuere magna varius posuere. Etiam et velit in leo elementum ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed at dignissim magna, a pretium nisl. Curabitur eget erat pretium, egestas mauris sit amet, pharetra arcu. Vestibulum vehicula eleifend erat, nec facilisis arcu mattis eu. Proin sed neque tempus eros ornare semper eu eu augue. Vivamus blandit arcu sit amet massa congue iaculis. Nulla ornare, justo ac pellentesque malesuada, erat leo ultricies ante, quis pellentesque nulla eros non risus. Phasellus vestibulum et leo at varius. Morbi ultricies semper erat vel faucibus. Praesent a sem est. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet pellentesque urna, a tristi
+                    que diam. Nulla posuere tempus turpis, id posuere magna varius posuere. Etiam et velit in leo elemen
+                    tum ultricies. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed at dignissim magna,
+                    a pretium nisl. Curabitur eget erat pretium, egestas mauris sit amet, pharetra arcu. Vestibulum vehi
+                    cula eleifend erat, nec facilisis arcu mattis eu. Proin sed neque tempus eros ornare semper eu eu au
+                    gue. Vivamus blandit arcu sit amet massa congue iaculis. Nulla ornare, justo ac pellentesque malesua
+                    da, erat leo ultricies ante, quis pellentesque nulla eros non risus. Phasellus vestibulum et leo at
+                    varius. Morbi ultricies semper erat vel faucibus. Praesent a sem est.
                 </p>
                 <footer>
                     <a href="algo.html">See why?</a>
                 </footer>
             </article>
-            <!--<ul id="galeria">
+            <ul id="galeria">
                 <li><div><img src="private/img/web/p1.jpg" height="100"/><br>Imagen 1</div></li>
                 <li><div><img src="private/img/web/p2.jpg" height="100"/><br>Imagen 2</div></li>
                 <li><div><img src="private/img/web/p3.jpg" height="100"/><br>Imagen 3</div></li>
                 <li><div><img src="private/img/web/p4.jpg" height="100"/><br>Imagen 4</div></li>
                 <li><div><img src="private/img/web/p5.jpg" height="100"/><br>Imagen 5</div></li>
                 <li><div><img src="private/img/web/p6.jpg" height="100"/><br>Imagen 6</div></li>
-                <li><div><img src="private/img/web/p1.jpg" height="100"/><br>Imagen 1</div></li>
-                <li><div><img src="private/img/web/p3.jpg" height="100"/><br>Imagen 3</div></li>
-                <li><div><img src="private/img/web/p4.jpg" height="100"/><br>Imagen 4</div></li>
-                <li><div><img src="private/img/web/p5.jpg" height="100"/><br>Imagen 5</div></li>
-                <li><div><img src="private/img/web/p6.jpg" height="100"/><br>Imagen 6</div></li>
-                <li><div><img src="private/img/web/p2.jpg" height="100"/><br>Imagen 2</div></li>
-                <li><div><img src="private/img/web/p3.jpg" height="100"/><br>Imagen 3</div></li>
-                <li><div><img src="private/img/web/p4.jpg" height="100"/><br>Imagen 4</div></li>
-                <li><div><img src="private/img/web/p5.jpg" height="100"/><br>Imagen 5</div></li>
-                <li><div><img src="private/img/web/p6.jpg" height="100"/><br>Imagen 6</div></li>
-                <li><div><img src="private/img/web/p1.jpg" height="100"/><br>Imagen 1</div></li>
-                <li><div><img src="private/img/web/p3.jpg" height="100"/><br>Imagen 3</div></li>
-                <li><div><img src="private/img/web/p4.jpg" height="100"/><br>Imagen 4</div></li>
-                <li><div><img src="private/img/web/p5.jpg" height="100"/><br>Imagen 5</div></li>
-                <li><div><img src="private/img/web/p1.jpg" height="100"/><br>Imagen 1</div></li>
-            </ul>-->
+            </ul>
         </div>
         <footer></footer>
     </body>
